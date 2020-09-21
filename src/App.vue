d546ca67824e46d0c8917b65f5b153ca09325f34
WXD <wxdie@chint.com>
2020-09-21 18:56:02 +0800

    historynoted

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,41 @@
 <template>
-  <div id="app">
-    <div id="nav">
-
-    </div>
-    <router-view/>
+  <div>
+    <canvas id="canvas" width="300" height="300"></canvas>
+    <button id="button" @click="click">点击移动</button>
   </div>
 </template>
 
+<script>
+
+export default {
+  mounted(){
+    var c = document.getElementById("canvas").getContext("2d");
+    console.log(c)
+    c.fillStyle = 'skyblue';
+    c.fillRect(0,0,300,300);
+  },
+  methods: {
+    click(){
+
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+#canvas {
+  border: none;
+  position: relative;
+  left: 600px;
+  top: 200px;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#button{
+  border: none;
+  width: 100px;
+  height: 50px;
+  font-size: 18px;
+  position: relative;
+  left: 400px;
+  top: 280px;
 }
 </style>
